4da6710536c7f55ad603232476c6e9d64a6950f1
Andrew Wells <alw331@gmail.com>
2020-04-17 17:05:35 -0500

    prepare server for deploy

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,34 +1,50 @@
-import "dotenv/config";
-import "reflect-metadata";
-import { createConnection } from "typeorm";
-import express from "express";
-import { ApolloServer } from "apollo-server-express";
-import { buildSchema } from "type-graphql";
-import { UserResolver } from "./UserResolver";
-import cookieParser from "cookie-parser";
-import { verify } from "jsonwebtoken";
-import { User } from "./entity/User";
-import { createAccessToken, createRefreshToken } from "./auth";
-import { sendRefreshToken } from "./sendRefreshToken";
-import cors from "cors";
+import 'dotenv/config';
+import 'reflect-metadata';
+import {createConnection, ConnectionOptions} from 'typeorm';
+import express from 'express';
+import {ApolloServer} from 'apollo-server-express';
+import {buildSchema} from 'type-graphql';
+import {UserResolver} from './UserResolver';
+import cookieParser from 'cookie-parser';
+import {verify} from 'jsonwebtoken';
+import {User} from './entity/User';
+import {createAccessToken, createRefreshToken} from './auth';
+import {sendRefreshToken} from './sendRefreshToken';
+import cors from 'cors';
 
 (async () => {
-  await createConnection();
+  //await createConnection();
+
+  await createConnection(<ConnectionOptions>{
+    type: 'postgres',
+
+    // We need add the extra SSL to use heroku on localhost
+    extra: {
+      ssl: process.env.DATABASE_SSL === 'true',
+    },
+
+    // Change the next line to use the Heroku postgresql from other environment like localhost, remenber that heroku changes this data periodically for security reasons
+    url: process.env.DATABASE_URL,
+
+    entities: ['entity/**/*.js'],
+    subscribers: [],
+    synchronize: process.env.DATABASE_SYNC === 'true',
+  });
 
   const app = express();
   app.use(
     cors({
-      origin: "http://localhost:3000",
+      origin: process.env.WEB_URL,
       credentials: true,
-    })
+    }),
   );
   app.use(cookieParser());
 
-  app.get("/health", (_, res) => {
+  app.get('/health', (_, res) => {
     res.send(200);
   });
 
-  app.post("/refresh_token", async (req, res) => {
+  app.post('/refresh_token', async (req, res) => {
     const token = req.cookies.sid;
     if (!token) {
       return res.status(401).send();
@@ -45,21 +61,24 @@ import cors from "cors";
       // Refresh refresh token if user is active
       sendRefreshToken(res, createRefreshToken(user!.id, user!.tokenVersion));
 
-      return res.json({ accessToken: createAccessToken(user!.id) });
+      return res.json({accessToken: createAccessToken(user!.id)});
     } catch (err) {
       return res.status(401).send();
     }
   });
 
-  const schema = await buildSchema({ resolvers: [UserResolver] });
+  const schema = await buildSchema({resolvers: [UserResolver]});
 
   const apolloServer = new ApolloServer({
     schema,
-    context: ({ req, res }) => ({ req, res }),
+    context: ({req, res}) => ({req, res}),
+    introspection: true,
+    playground: true,
   });
-  apolloServer.applyMiddleware({ app, cors: false });
+  apolloServer.applyMiddleware({app, cors: false});
 
-  app.listen(4000, () => {
-    console.log("server listening on http://localhost:4000");
+  const port = process.env.PORT || '4000';
+  app.listen(parseInt(port), () => {
+    console.log(`server listening on http://localhost:${port}`);
   });
 })();
